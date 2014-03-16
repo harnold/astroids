09b802bab824878db9c35825186179582a036d07
Holger Arnold <holgerar@gmail.com>
2014-03-16 13:04:10 +0100

    Add sprite.h, sprite.c

---
+++ src/sprite.c
@@ -0,0 +1,92 @@
+#include "sprite.h"
+
+#include <stdlib.h>
+
+DEFINE_ALLOCATOR(sprite, struct sprite, ALLOC_DEFAULT_BLOB_SIZE);
+
+void init_sprite_class(struct sprite_class *class, struct image *image,
+                      int width, int height, int num_frames,
+                      float origin_x, float origin_y)
+{
+    class->image = image;
+    class->width = width;
+    class->height = height;
+    class->num_frames = num_frames;
+    class->origin_x = origin_x;
+    class->origin_y = origin_y;
+}
+
+void init_sprite(struct sprite *sprite, const struct sprite_class *class,
+                 float x, float y, int z, int frame)
+{
+    *((struct sprite_class *) sprite) = *class;
+    sprite->x = x;
+    sprite->y = y;
+    sprite->z = z;
+    sprite->frame = frame;
+    sprite->anim = NULL;
+    sprite->anim_start_time = 0.0f;
+    elist_link(&sprite->link, &sprite->link);
+}
+
+void destroy_sprite(struct sprite *sprite)
+{
+    elist_remove(&sprite->link);
+}
+
+struct sprite *create_sprite(const struct sprite_class *class, float x, float y,
+                             int z, int frame)
+{
+    struct sprite *sprite = alloc_sprite();
+    init_sprite(sprite, class, x, y, z, frame);
+    return sprite;
+}
+
+void delete_sprite(struct sprite *sprite)
+{
+    destroy_sprite(sprite);
+    free_sprite(sprite);
+}
+
+void sprite_set_animation(struct sprite *sprite, const struct animation *anim,
+                          float start_time)
+{
+    sprite->anim = anim;
+    sprite->anim_start_time = start_time;
+}
+
+void sprite_update(struct sprite *sprite, float t, float dt)
+{
+    const struct animation *anim = sprite->anim;
+
+    if (anim == NULL)
+        return;
+
+    int nframe = (int) ((t - sprite->anim_start_time) * anim->fps);
+
+    switch (anim->type) {
+
+    case ANIM_ROTATE_FORWARD:
+        sprite->frame = anim->start_frame +
+            nframe % (anim->end_frame - anim->start_frame + 1);
+        break;
+
+    case ANIM_ROTATE_BACKWARD:
+        sprite->frame = anim->start_frame -
+            nframe % (anim->start_frame - anim->end_frame + 1);
+        break;
+
+    case ANIM_ONCE_FORWARD:
+        sprite->frame = min(anim->start_frame + nframe, anim->end_frame);
+        break;
+
+    case ANIM_ONCE_BACKWARD:
+        sprite->frame = max(anim->start_frame - nframe, anim->end_frame);
+        break;
+    }
+}
+
+void sprite_cleanup(void)
+{
+    free_all_sprite_allocs();
+}
---
+++ src/sprite.h
@@ -0,0 +1,85 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include "alloc.h"
+#include "elist.h"
+
+#include <stdint.h>
+
+struct image;
+
+enum animation_type {
+    ANIM_ROTATE_FORWARD,
+    ANIM_ROTATE_BACKWARD,
+    ANIM_ONCE_FORWARD,
+    ANIM_ONCE_BACKWARD
+};
+
+struct animation {
+    enum animation_type type;
+    float fps;
+    int start_frame;
+    int end_frame;
+};
+
+struct sprite_class {
+    struct image *image;
+    int width;
+    int height;
+    int num_frames;
+    float origin_x;
+    float origin_y;
+};
+
+struct sprite {
+    struct image *image;
+    int width;
+    int height;
+    int num_frames;
+    float origin_x;
+    float origin_y;
+    float x;
+    float y;
+    int z;
+    int frame;
+    const struct animation *anim;
+    float anim_start_time;
+    struct elist_node link;
+};
+
+DECLARE_ALLOCATOR(sprite, struct sprite);
+
+#define sprite_list_for_each(__sprite, __list) \
+    elist_for_each_elem((__sprite), (__list), struct sprite, link)
+
+#define sprite_list_get(__node) \
+    elist_get(__node, struct sprite, link)
+
+static inline int sprite_get_x(const struct sprite *sprite);
+static inline int sprite_get_y(const struct sprite *sprite);
+
+void init_sprite_class(struct sprite_class *class, struct image *image,
+                       int width, int height, int num_frames,
+                       float origin_x, float origin_y);
+void init_sprite(struct sprite *sprite, const struct sprite_class *class,
+                 float x, float y, int z, int frame);
+void destroy_sprite(struct sprite *sprite);
+struct sprite *create_sprite(const struct sprite_class *class, float x, float y,
+                             int z, int frame);
+void delete_sprite(struct sprite *sprite);
+void sprite_set_animation(struct sprite *sprite, const struct animation *anim,
+                          float start_time);
+void sprite_update(struct sprite *sprite, float t, float dt);
+void sprite_cleanup(void);
+
+static inline int sprite_get_x(const struct sprite *sprite)
+{
+    return (int) (sprite->x - sprite->origin_x + 0.5f);
+}
+
+static inline int sprite_get_y(const struct sprite *sprite)
+{
+    return (int) (sprite->y - sprite->origin_y + 0.5f);
+}
+
+#endif

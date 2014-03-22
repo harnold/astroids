46441de0754fcf0f9e511455dc4cb192cc0a20fb
Holger Arnold <holgerar@gmail.com>
2014-03-22 17:38:45 +0100

    Add asteroid.h, asteroid.c

---
+++ src/asteroid.c
@@ -0,0 +1,68 @@
+#include "asteroid.h"
+#include "res.h"
+#include "sprite.h"
+#include "world.h"
+
+const struct animation asteroid_animations[] = {
+    { ANIM_ROTATE_FORWARD, 60, 0, 19 },
+    { ANIM_ROTATE_FORWARD, 60, 0, 19 },
+    { ANIM_ROTATE_FORWARD, 60, 0, 19 },
+};
+
+const struct sprite_class asteroid_classes[] = {
+    { &asteroid_big_image, 48, 48, 20, 24.0f, 24.0f },
+    { &asteroid_mid_image, 32, 32, 20, 16.0f, 16.0f },
+    { &asteroid_small_image, 16, 16, 20, 8.0f, 8.0f }
+};
+
+void init_asteroid(struct asteroid *ast, enum asteroid_type type,
+                   float x, float y, float vx, float vy, int z, float time)
+{
+    ast->type = type;
+    ast->world_x = x;
+    ast->world_y = y;
+    ast->world_vx = vx;
+    ast->world_vy = vy;
+
+    struct sprite *spr = create_sprite(&asteroid_classes[type],
+                                       world_to_screen_x(x),
+                                       world_to_screen_y(y), z, 0);
+
+    sprite_set_animation(spr, &asteroid_animations[type], time);
+
+    ast->sprite = spr;
+    elist_link(&ast->link, &ast->link);
+}
+
+void destroy_asteroid(struct asteroid *ast)
+{
+    elist_remove(&ast->link);
+    delete_sprite(ast->sprite);
+}
+
+struct asteroid *create_asteroid(enum asteroid_type type, float x, float y,
+                                 float vx, float vy, int z, float time)
+{
+    struct asteroid *ast = alloc_asteroid();
+    init_asteroid(ast, type, x, y, vx, vy, z, time);
+    return ast;
+}
+
+void delete_asteroid(struct asteroid *ast)
+{
+    destroy_asteroid(ast);
+    free_asteroid(ast);
+}
+
+void asteroid_set_type(struct asteroid *ast, enum asteroid_type type, float time)
+{
+    ast->type = type;
+    const struct sprite_class *class = &asteroid_classes[type];
+    *((struct sprite_class *) ast->sprite) = *class;
+    sprite_set_animation(ast->sprite, &asteroid_animations[type], time);
+}
+
+void asteroid_cleanup(void)
+{
+    free_all_asteroid_allocs();
+}
---
+++ src/asteroid.h
@@ -0,0 +1,49 @@
+#ifndef ASTEROID_H
+#define ASTEROID_H
+
+#include "alloc.h"
+#include "elist.h"
+
+#include <stdbool.h>
+
+struct animation;
+struct sprite_class;
+struct sprite;
+
+extern const struct animation asteroid_animations[];
+extern const struct sprite_class asteroid_classes[];
+
+enum asteroid_type {
+    BIG_ASTEROID,
+    MID_ASTEROID,
+    SMALL_ASTEROID
+};
+
+struct asteroid {
+    enum asteroid_type type;
+    float world_x;
+    float world_y;
+    float world_vx;
+    float world_vy;
+    struct sprite *sprite;
+    struct elist_node link;
+};
+
+DECLARE_ALLOCATOR(asteroid, struct asteroid);
+
+#define asteroid_list_for_each(__asteroid, __list) \
+    elist_for_each_elem((__asteroid), (__list), struct asteroid, link)
+
+#define asteroid_list_get(__node) \
+    elist_get(__node, struct asteroid, link)
+
+void init_asteroid(struct asteroid *ast, enum asteroid_type type,
+                   float x, float y, float vx, float vy, int z, float time);
+void destroy_asteroid(struct asteroid *ast);
+struct asteroid *create_asteroid(enum asteroid_type type, float x, float y,
+                                 float vx, float vy, int z, float time);
+void delete_asteroid(struct asteroid *ast);
+void asteroid_set_type(struct asteroid *ast, enum asteroid_type type, float time);
+void asteroid_cleanup(void);
+
+#endif

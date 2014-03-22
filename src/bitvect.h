e7925f51a56af2844d8bc99b3c2219a2eda47d43
Holger Arnold <holgerar@gmail.com>
2014-03-22 13:52:02 +0100

    Add bitvect.h

---
+++ src/bitvect.h
@@ -0,0 +1,22 @@
+#ifndef BITVECT_H
+#define BITVECT_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+static inline void vbitvect_set_bit(volatile uint8_t *v, unsigned bit)
+{
+    *(v + bit / 8) |= 1 << (bit % 8);
+}
+
+static inline void vbitvect_clear_bit(volatile uint8_t *v, unsigned bit)
+{
+    *(v + bit / 8) &= ~(1 << (bit % 8));
+}
+
+static inline bool vbitvect_test_bit(volatile uint8_t *v, unsigned bit)
+{
+    return *(v + bit / 8) & (1 << (bit % 8)) != 0;
+}
+
+#endif

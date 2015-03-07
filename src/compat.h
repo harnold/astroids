7abf11886517083a8ed0851e4fb7ef601a8ccefc
Holger Arnold <holgerar@gmail.com>
2015-03-07 01:02:59 +0100

    Indent some conditional macro definitions

--- src/compat.h
+++ src/compat.h
@@ -30,38 +30,32 @@ static inline float confine_float(float x, float min, float max)
 #define UNUSED(x)			(void) (x)
 
 #ifndef __unique_identifier
-
-#define __concat1(a, b)                 a##b
-#define __concat(a, b)                  __concat1(a, b)
-#define __unique_identifier(prefix)     __concat(prefix, __LINE__)
-
+  #define __concat1(a, b)               a##b
+  #define __concat(a, b)                __concat1(a, b)
+  #define __unique_identifier(prefix)   __concat(prefix, __LINE__)
 #endif /* __unique_identifier */
 
 #if __STDC_VERSION__ < 201000
 
-#ifndef static_assert
+  #ifndef static_assert
+    #define _Static_assert(cond, msg) \
+            typedef struct { \
+                int __static_assert_failure: (cond) ? 1 : -1; \
+            } __unique_identifier(__static_assert_);
 
-#define _Static_assert(cond, msg) \
-        typedef struct { \
-            int __static_assert_failure: (cond) ? 1 : -1; \
-        } __unique_identifier(__static_assert_);
+    #define static_assert   _Static_assert
+  #endif /* static_assert */
 
-#define static_assert   _Static_assert
-
-#endif /* static_assert */
-
-#ifndef alignof
-
-#define _Alignof(type)  offsetof(struct { char a; type b; }, b)
-#define alignof(type)   _Alignof(type)
-
-#endif /* alignof */
+  #ifndef alignof
+    #define _Alignof(type) offsetof(struct { char a; type b; }, b)
+    #define alignof(type) _Alignof(type)
+  #endif /* alignof */
 
 #endif /* C standard < C11 */
 
 #if defined(__WATCOMC__)
 
-  #define PACKED_STRUCT   _Packed struct
+  #define PACKED_STRUCT	_Packed struct
 
 #else /* Not Watcom C */
 
